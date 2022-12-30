4609445bd7f4e43beb8fa3834efbcfddb273c97a
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2022-12-30 11:46:03 +0530

    Profile screen UI completed...

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,10 +1,93 @@
-import { View } from "react-native";
+import { View, Text, StyleSheet, Image, TextInput } from "react-native";
+import Header from '../components/Header';
+import image1 from '../assets/image.jpg';
+import { MaterialIcons } from '@expo/vector-icons';
+import { COLORS } from "../components/constants";
 
 function ProfileScreen(){
     return(
-        <View>
-            <Text>Hello</Text>
+        <View style={styles.container}>
+            <Header headerText={"My Account"} />
+            <View style={styles.imgContainer}>
+                <Image
+                    source={image1}
+                    style={styles.imgStyle} 
+                    />
+                <View style={styles.badgeStyle}><MaterialIcons name="edit" size={24} color={COLORS.white} /></View>
+            </View>
+            <Field name="Name" />
+            <Field name="Contact" />
+            <Field name="Email" />
+            <Field name="Home Address" />
+            <Field name="Profession" />
+            <Report_Button />
         </View>
     );
 }
+function Field({name}){
+    return(
+        <View style={styles.fieldContainer}>
+            <Text>{name}</Text>
+            <TextInput
+                style={styles.inputStyle}
+                cursorColor={COLORS.gray}
+            />
+        </View>
+    );
+}
+
+function Report_Button(){
+    return(
+        <View style={styles.report_btn}>
+            <Text> Logout </Text>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container:{
+        flex: 1
+    },
+    
+    imgContainer:{
+        flexDirection: 'row',
+        alignSelf: 'center',
+    },
+    imgStyle:{
+        width: 120,
+        height: 120,
+        borderRadius: 100,
+        marginTop: 10
+
+    },
+    badgeStyle:{
+        position: 'absolute',
+        bottom: 0,
+        right: 0,
+        backgroundColor: COLORS.secondary,
+        borderRadius: 50,
+        padding: 5
+    },
+    inputStyle:{
+        backgroundColor: COLORS.lightGray,
+        borderRadius: 4,
+        paddingLeft: 5
+    },
+    fieldContainer:{
+        paddingHorizontal: 15,
+        marginTop: 10
+    },
+    report_btn:{
+        marginTop: 10,
+        paddingVertical: 10,
+        width: '90%',
+        position: 'absolute',
+        bottom: 10,
+        alignSelf: 'center',
+        alignItems: 'center',
+        backgroundColor: "#f8fafc",
+        borderColor: COLORS.red,
+        borderWidth: 1,
+        borderRadius: 5,
+    },
+});
 export default ProfileScreen;
