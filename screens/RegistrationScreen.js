dc7186f9970bb11a734e838144c04b58d30285a3
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2023-01-23 13:52:55 +0530

    Components added...

--- screens/RegistrationScreen.js
+++ screens/RegistrationScreen.js
@@ -1,15 +1,53 @@
 import { StyleSheet, Text, View } from "react-native";
+import { COLORS } from "../components/constants";
 
 function RegistrationScreen(){
     return(
         <View style={styles.container}>
-            <Text>Hello Registration Screen</Text>
+            <Text style={{color: COLORS.white, fontSize: 30, fontWeight: '400' }}>Registration</Text>
+            <RegistrationProgress/>
+            <Register/>
+        </View>
+    );
+}
+function RegistrationProgress(){
+    return(
+    <View>
+        <Text style={{fontSize: 16, color: COLORS.white}} >Register  Personal  SkillSets  Documents</Text>
+    </View>
+    );
+}
+function Register(){
+    return(
+        <View style={{ backgroundColor: COLORS.white,padding: 16 }} >
+            <Text style={{fontSize: 16,color: COLORS.gray, }}>What describes you best?</Text>
+        </View>
+    );
+}
+function Personal(){
+    <View>
+        <Text>Personal</Text>
+    </View>
+}
+function SkillSets(){
+    return(
+        <View>
+            <Text>Skill Sets</Text>
+        </View>
+    );
+}
+function Documents(){
+    return(
+        <View>
+            <Text>Documents...</Text>
         </View>
     );
 }
 const styles = StyleSheet.create({
     container:{
         flex: 1,
+        backgroundColor: COLORS.black,
+
     }
 });
 export default RegistrationScreen;
