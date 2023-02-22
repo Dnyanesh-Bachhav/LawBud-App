8628b827d9642b8a62f92f2589538108742bb5f2
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2023-02-22 18:54:00 +0530

    added firebase...

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBsAznu4qMfAX5tVY2w5g7oYXB1x8VOkd8",
+  authDomain: "lawbud-chatapp.firebaseapp.com",
+  projectId: "lawbud-chatapp",
+  storageBucket: "lawbud-chatapp.appspot.com",
+  messagingSenderId: "939772231796",
+  appId: "1:939772231796:web:2a7a59927ce9ed7b33d121"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
