54783164b42e23e1c6ea3bbc67ae8f1b6dd94c07
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2023-04-29 23:13:44 +0530

    OTP authentication added....

--- firebase.js
+++ firebase.js
@@ -4,11 +4,14 @@ import Constants from "expo-constants";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: Constants.manifest.extra.apiKey,
   authDomain: Constants.manifest.extra.authDomain,
   projectId: Constants.manifest.extra.projectId,
@@ -18,7 +21,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage(app);
 export const firestore = getFirestore();
